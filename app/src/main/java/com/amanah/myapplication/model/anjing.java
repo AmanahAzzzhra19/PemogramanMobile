a5f6ab0ec35fcde73ebaf667c39e976647bcc6db
amanahazzahra <amanahazzahra@gmail.com>
2020-11-25 14:58:52 +0800

    tugas

---
+++ app/src/main/java/com/amanah/myapplication/model/anjing.java
@@ -0,0 +1,8 @@
+package com.amanah.myapplication.model;
+
+public class anjing extends hewan{
+    public anjing(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Anjing",ras,asal,deskripsi,drawableRes);
+    }
+}
+
---
+++ app/src/main/java/com/amanah/myapplication/model/sapi.java
@@ -0,0 +1,7 @@
+package com.amanah.myapplication.model;
+
+public class sapi extends hewan {
+    public sapi(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Anjing", ras, asal, deskripsi, drawableRes);
+    }
+}
